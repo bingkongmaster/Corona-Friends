d9afcb294ff007b17a7aa89b94a6cbd14b5adec7
bingkongmaster <bingkongmaster@gmail.com>
2020-06-19 09:22:27 +0900

    Added Kakao map API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import {Helmet} from "react-helmet";
 
-export default function Home() {
+export default class Home extends React.Component {
+  render() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Dong Hwan Kim" />
+    <div style={{ color: `purple` }}> //later change to layout
+    const myExtScript = require('../scripts/kakaomap')
       <p>Welcome to donghwankim.com!</p>
       <p>Powered by Gatsby</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <div id="map" style="width:500px;height:400px;"></div>
+      <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=3199e8f198aff9d5aff73000faae6608"></script>
+      <script
+        dangerouslySetInnerHTML={{ __html:
+          `
+          var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+              mapOption = {
+                  center: new kakao.maps.LatLng(37.56591, 126.97894), // 지도의 중심좌표
+                  level: 4, // 지도의 확대 레벨
+                  mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+              }; 
+      
+          // 지도를 생성한다 
+          var map = new kakao.maps.Map(mapContainer, mapOption); `
+        }}
+      />
     </div>
-  )
+  )}
 }
