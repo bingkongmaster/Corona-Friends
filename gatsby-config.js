44032de118d19a15c66ee1e5c2d47d2178477b38
bingkongmaster <bingkongmaster@gmail.com>
2020-06-24 08:09:35 +0900

    Added site favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/corona2.png`,
+      },
+    },
+  ],
 }
